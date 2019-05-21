16a6b4de17187f6d71a92f199d304e17564f5a8d
hhoo1645 <43479514+hhoo1645@users.noreply.github.com>
2019-05-21 03:48:22 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,16 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
 var prefix = "-";
 var adminprefix = '-'
 
